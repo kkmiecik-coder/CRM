5b4dcb9f491fc13f92775cb0fa08f8e1fc0476d3
kkmiecik-coder <konrad.kmiecik@rsholding.com.pl>
2026-02-25 16:23:43 +0100

    Refactor code structure for improved readability and maintainability

--- migrations/006_fix_avatar_paths.sql
+++
@@ -1,9 +0,0 @@
--- Fix avatar paths: add 'images/avatars/' prefix to paths that are missing it
--- This fixes custom avatars stored as 'custom/...' instead of 'images/avatars/custom/...'
--- and default avatars stored as 'default_avatars/...' instead of 'images/avatars/default_avatars/...'
-
-UPDATE users
-SET avatar_path = CONCAT('images/avatars/', avatar_path)
-WHERE avatar_path IS NOT NULL
-  AND avatar_path != ''
-  AND avatar_path NOT LIKE 'images/avatars/%';
