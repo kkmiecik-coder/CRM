65d8758d8b244ff9b7758fa629ff3531c18fe5c9
kkmiecik-coder <konrad.kmiecik@rsholding.com.pl>
2026-04-29 12:56:15 +0200

    feat(production): przyciski drukowania etykiet w UI stanowisk (formatowanie + pakowanie)

--- modules/production/routers/stations/interfaces.py
+++ modules/production/routers/stations/interfaces.py
@@ -663,6 +663,7 @@ def formatting_station():
                     'shape_svg': product.shape_svg,
                     'lamella_direction': product.lamella_direction,
                     'has_visual_data': bool(product.shape_svg or product.edge_svg),
+                    'label_print_count': product.label_print_count or 0,
                 }
 
                 products.append(product_data)
@@ -1136,6 +1137,7 @@ def packaging_station():
                     'shape_svg': product.shape_svg,
                     'lamella_direction': product.lamella_direction,
                     'has_visual_data': bool(product.shape_svg or product.edge_svg),
+                    'label_print_count': product.label_print_count or 0,
                 }
 
                 products.append(product_data)
